892a6859f5c6ad3cfe997da3fab71473452d01d4
Şenay Şentürk <ssenturk@tp-otc.com>
2023-06-19 14:28:36 +0300

    add/remove list button added

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Category } from '../models/category';
+import { CategoryRepository } from '../models/category.repository';
 
 @Component({
   selector: 'app-category',
@@ -7,10 +8,10 @@ import { Category } from '../models/category';
   styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent {
-  categories: Category[] = [
-    { id: 1, name: 'Macera' },
-    { id: 2, name: 'Romantik' },
-    { id: 3, name: 'Bilim Kurgu' },
-    { id: 4, name: 'Fantastik' },
-  ];
+  categories: Category[];
+  categoryRespository: CategoryRepository;
+  constructor() {
+    this.categoryRespository = new CategoryRepository();
+    this.categories = this.categoryRespository.getCategory();
+  }
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -9,11 +9,28 @@ import { MovieRepository } from '../models/movie.repository';
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[];
+  movieList: any = [];
   movieRepository: MovieRepository;
-  searchMovie: string = 'movie-1';
+  searchMovie: string = '';
+  btn: any;
   constructor() {
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getMovies();
   }
   ngOnInit(): void {}
+  addRemoveList($event: any, movieId: number) {
+    const index = this.movieList.indexOf(movieId);
+    if (index === -1) {
+      this.movieList.push(movieId);
+    } else {
+      this.movieList.splice(index, 1);
+    }
+
+    console.log(this.movieList);
+
+    return this.movieList;
+  }
+  isInList(movieId: number) {
+    return this.movieList.includes(movieId);
+  }
 }
