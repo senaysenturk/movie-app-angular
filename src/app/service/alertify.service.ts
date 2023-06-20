42905b4a2c96a19414a4fbce1dbd2f00470229a7
Şenay Şentürk <ssenturk@tp-otc.com>
2023-06-20 10:33:29 +0300

    service created for alertify

--- src/app/service/alertify.service.ts
+++ src/app/service/alertify.service.ts
@@ -9,11 +9,9 @@ export class AlertifyService {
   success(message: string) {
     alertify.success(message);
   }
-
   error(message: string) {
     alertify.error(message);
   }
-
   warning(message: string) {
     alertify.warning(message);
   }
